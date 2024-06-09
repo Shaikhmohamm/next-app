935677e297c6c2c6cd4a12cd1c5c1eb2593286b0
zoha shaikh <zoha@pleximus.net>
2024-06-10 01:29:52 +0530

    first commit

---
+++ pages/blog/first.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const first = () => {
+  return (
+    <h1>Blog page 1</h1>
+  )
+}
+
+export default first
---
+++ pages/blog/second.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const second = () => {
+  return (
+    <h1>Blog page 2</h1>
+  )
+}
+
+export default second
